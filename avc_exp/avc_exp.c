bc8f3c10e9653de2dd88fcda2ab60446872a25a8
jeff <163jogh@163.com>
2018-02-25 02:17:52 +0800

    [avc_exp] trace ff_h264_init_cabac_states() in FFMPEG -------------------------------------------
    verify: cabac_state[ctxIdx] = 2 * pStateIdx[ctxIdx] + valMPS
    cabac_state[] defined in ffmpeg H264SliceContext.cabac_state[1024]
    pStateIdx defined in <9.3.1.1, H264 spec (2005,03)>

---
+++ avc_exp/avc_exp.c
@@ -0,0 +1,59 @@
+/*****************************************************************************
+ * Copyright 2014 Jeff <ggjogh@gmail.com>
+ *****************************************************************************
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*****************************************************************************/
+
+#include <string.h>
+#include "sim_opt.h"
+#include "avc_exp.h"
+
+
+int main(int argc, char **argv)
+{
+    int i=0, j = 0;
+    int exit_code = 0;
+    
+    typedef struct test_module {
+        const char *name; 
+        int (*func)(int argc, char **argv);
+        const char *help;
+    } test_module_t;
+    
+    const static test_module_t sub_main[] = {
+        {"cabac",     cabac_test,    "yuv fmt convertor"},
+    };
+
+    xlog_init(SLOG_PRINT);
+    i = arg_parse_xlevel(1, argc, argv);
+
+    if (i>=argc) {
+        xprint("No module specified. ");
+    } else {
+        for (j=0; j<ARRAY_SIZE(sub_main); ++j) {
+            if (0==strcmp(argv[i], sub_main[j].name)) {
+                return sub_main[j].func(argc-i, argv+i);
+            }
+        }
+        if (strcmp(argv[i], "-h") && strcmp(argv[i], "--help")) {
+            xprint("`%s` is not support. ", argv[i]);
+        }
+    }
+    
+    xprint("Use the following modules:\n");
+    for (j=0; j<ARRAY_SIZE(sub_main); ++j) {
+        xprint("\t%4s - %s\n", sub_main[j].name, sub_main[j].help);
+    }
+    
+    return exit_code;
+}
